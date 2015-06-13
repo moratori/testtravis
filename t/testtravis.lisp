7375f2adf9a0c547ec3eaa80136c0d7d0bc4eb8b
moratori <hoko1993@yahoo.co.jp>
2015-06-13 18:14:22 +0900

    テストを追加した

--- t/testtravis.lisp
+++ t/testtravis.lisp
@@ -7,8 +7,12 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :testtravis)' in your Lisp.
 
-(plan nil)
+
+(plan 2)
 
 ;; blah blah blah.
+
+(is (fact 5) 120)
+(isnt (fact 4) 33)
 
 (finalize)

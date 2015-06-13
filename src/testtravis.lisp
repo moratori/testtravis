cbc06140e5095de730ba91a6eaa325b78ba30a49
moratori <hoko1993@yahoo.co.jp>
2015-06-13 18:15:33 +0900

    テストの対象となる関数を書いた

--- src/testtravis.lisp
+++ src/testtravis.lisp
@@ -1,6 +1,15 @@
 (in-package :cl-user)
 (defpackage testtravis
-  (:use :cl))
+  (:use :cl)
+  (:export 
+    :fact
+    )
+  )
 (in-package :testtravis)
 
 ;; blah blah blah.
+
+
+(defun fact (n)
+  (if (zerop n)
+    1 (* n (fact (1- n)))))
